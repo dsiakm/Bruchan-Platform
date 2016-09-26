5598f5bd84a886c1739ada4b2f6c4f1387e7a06e
dsiakm <dsiak.mondala@gmail.com>
2016-09-26 16:54:11 -0300

    GUI now shows your current weapon and spell

---
+++ Assets/Scripts/GUI/ShowCurrentMeele.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowCurrentMeele : MonoBehaviour {
+
+	Image image;
+	public Sprite basicWeapon;
+
+	void Start () {
+		image = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		string weapon = PlayerState.playerState.GetActiveWeapon ();
+
+		if (weapon == "basicWeapon"){
+			image.sprite = basicWeapon;
+		}
+	}
+}
---
+++ Assets/Scripts/GUI/ShowCurrentSpell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowCurrentSpell : MonoBehaviour {
+
+	Image image;
+	public Sprite basicSpell, fireSpell;
+
+	void Start () {
+		image = GetComponent<Image> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		string spell = PlayerState.playerState.GetActiveSpell();
+
+		if (spell == "basicSpell"){
+			image.sprite = basicSpell;
+		}else if (spell == "fireSpell"){
+			image.sprite = fireSpell;
+		}
+	}
+}
